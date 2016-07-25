a6aefcb6f7ccaffaa8f9a0af8b28e3027f24da82
cseverino789 <corey.severino@gmail.com>
2016-07-24 21:34:51 -0400

    Add simple inline reverse logic to the basic get route.

--- node.js/server.js
+++ node.js/server.js
@@ -4,8 +4,14 @@ const express = require('express');
 const app = express();
 
 // Application
-app.get('/reverse/:in', function (req, res) {
-  res.send(req.params);
+app.get('/reverse/:inpt', function (req, res) {
+  var ans = '';
+  const inpt = req.params.inpt;
+
+  for (var i = inpt.length-1; i >= 0; i--)
+     ans += inpt[i];
+
+  res.send(ans);
 });
 
 const PORT = 8080
